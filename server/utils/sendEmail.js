c4fb38f9647d2325301672496d220aed547f276a
bellokhaleed84 <bellokhaleed84@gmail.com>
2026-08-16 04:40:05 -0700

    use verified domain for from address

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
 
   const { data, error } = await resend.emails.send({
-    from: "Danmus SMS <onboarding@resend.dev>",
+    from: "Danmus SMS <noreply@danmussms.com>",
     to,
     subject,
     html,
